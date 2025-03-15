1da04e983677f848621e7ff3f7d31c6cf60a94ac
DaijobuDes <git@daijobudes.net>
2025-03-15 21:26:41 +0800

    Specify each HTML file for vite to recognize in vite build

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from "path";
 
 export default defineConfig({
     plugins: [
@@ -7,6 +8,14 @@ export default defineConfig({
     ],
     build: {
         sourcemap: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                home: resolve(__dirname, "home.html"),
+                logout: resolve(__dirname, "logout.html"),
+                success: resolve(__dirname, "success.html"),
+            },
+        },
     },
     css: {
         devSourcemap: true,
